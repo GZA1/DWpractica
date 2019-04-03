c316978a150e0a82d18bb88f7e14eb6ae5e834f9
Khalifa96 <gsenov@gmail.com>
2019-04-03 17:51:53 +0200

    Newbd&shit
    Base de datos actualizada y un par de cosillas

---
+++ v2.0 dinamic/appdata/model/Empleado.php
@@ -0,0 +1,23 @@
+<?php 
+require_once("Console.php");
+require_once("Cliente.php");
+
+class Empleado extends Usuario {
+    private $photoPath;
+    private $active;
+    private $cargo;
+    private $isAdministrador;
+
+    public function __construct(){
+        parent::_construct();
+        $this ->id = spl_object_hash($this);
+        $this ->tipo = "empleado";
+    }
+
+
+    public addEmpleado
+
+}
+
+
+?>
--- v2.0 dinamic/htdocs/dw/inicio-sesion/sign-in.php
+++ v2.0 dinamic/htdocs/dw/inicio-sesion/sign-in.php
@@ -1,11 +1,8 @@
 <?php
-    require_once('/xampp/appdata/model/Console.php');
     require_once('/xampp/appdata/model/Usuario.php');
-    require_once('/xampp/appdata/model/Saldo.php');
+    require_once('/xampp/appdata/model/Cliente.php');
+    require_once('/xampp/appdata/model/Console.php');
 
-    session_start();
-
-    // $usuario = new Usuario($_SESSION['id']);
 
     if( $_SERVER['REQUEST_METHOD']=='GET') {
 ?>
@@ -18,88 +15,74 @@
         <meta name="robots" content="NoIndex, NoFollow">
         <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="../styles/style-shared.css">
-        <link rel="stylesheet" href="../styles/style-perfil.css">
+        <link rel="stylesheet" href="../styles/style-inicio-sesion.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <title>Página principal</title>
     </head>
     <body>
-
-        
         <div class="flex_cols" id="contenedor-inic">
-                <div style="height: 10vh; min-height: 70px; visibility: hidden"></div>
             <div id="logo-inic">
                 <a href="../main/index.php">
                     <img src="../img/logo_horizontal.png" width="100%">
                 </a>
             </div>
-            <div id="mainSection"> 
-                <div id="leftSect">
-                    <!-- <div id="userSummary"></div> -->
-                    <ul id="opcionesPerfil">
-                        <li class="pestañasPerfil">Cambiar contraseña</li>
-                        <!-- <li class="pestañasPerfil">Monedero</li> -->
-                    </ul>
-                </div>
-                <div id="rightSect">
-                    <h2 style="margin: 20px 0 12px 10px; border-bottom: 1px">Cambiar contraseña</h1>
-                    
-                    
-                    <div id="perfilConfig">
-                        
-                        
-                            <form method="post" id="cambiarPass">
-                                <label id="lPasswd">Introduzca su nueva contraseña</label>
-                                <input type="password" id="newPasswd" name="newPasswd">
-                                <label id="lPasswd2">Confirme su nueva contraseña</label>
-                                <input type="password" id="newPasswd2" name="newPasswd2"><br><br>
-                                <input id="boton-nueva-pass" type="submit" value="Cambiar contraseña">
-                            </form>                    
-                        
+            <div id="contenedor-form">
+                <form method="post">
+                    <h1 style="margin-top: 40px;">Iniciar sesión</h1>
+                    <label id="lId">Nombre de usuario o mail</label>
+                    <input type="text" id="id" name="id" name="id">
+                    <label id="lPass">Contraseña</label>
+                    <input type="password" id="passwd" name="passwd"><br><br>
+                    <input id="boton-inic-ses" type="submit" value="Iniciar sesión">
+                    <div class="flex_rows">
+                        <input id="recuerdame-checkbox" type="checkbox">
+                        <label>Recuérdame.</label><br><br>
                     </div>
-                </div>
-    
+                    <div  class="no-cuenta" >
+                        <a style="cursor: pointer;" href="sign-up.php">¿No tienes cuenta todavía?</a>
+                    </div>
+                </form>
             </div>
-        </div>
+            <script>
+                 (function($) {
+                    $('#miFormulario').submit(function() {
+                        $("#error").remove();
+                        var nombre = $("#id").val(),
+                            passwd = $("#passwd").val();
 
+                        var inputVal = [id, passwd],
+                            inputMessage = ["id", "passwd"],
+                            textId = ["#lId", "#lPasswd"];
 
-        <!-- <div class="flex_cols">
-            <div class="flex_rows" id="contenedor_main_ads">
-            <div id="sideContainer">
+                        for(var i=0;i<inputVal.length;i++){
+                            inputVal[i] = $.trim(inputVal[i]);
+                            console.log(inputVal[i]);
+                            if ( inputVal[i] == null || inputVal[i] === "") {
+                                console.log(inputVal[i] + ' incorrecto');
+                                invalidEntry(i);
+                                return false;
+                            }
+                        }
+                        return true;
+                        
+                        function invalidEntry(i) {
+                            console.log(textId[i] + ' incorrecto');
+                            $(textId[i]).after("<p id='error' style='font-size: 14px; color: red' > El campo " + inputMessage[i] + " no es válido.</p>");
+                        }
+                        function isEmail(email) {
+                            console.log(email);
+                            var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+                            return regex.test(email);
+                        }
+                    });
 
-            </div>
+                    $("input[type='text']").change(function() {
+                        $("#error").remove();
+                    });
 
-            <div class="mainContainer">
-                <div id="contenidoMain" class="flex_cols transition">
-
-
-                </div>
-            </div>
-          </div>
-        </div> -->
-        <script type="text/javascript">
-            $("document").ready(function() {
-
-                $("#c1").click(function(){
-                   $("#secondThing").fadeOut();
-                    $("#firstThing").fadeIn();
-                    $("#thirdThing").fadeOut();
-                });
-
-                $("#c2").click(function(){
-                   $("#secondThing").fadeIn();
-                    $("#firstThing").fadeOut();
-                    $("#thirdThing").fadeOut();
-                });
-
-                $("#c3").click(function(){
-                   $("#secondThing").fadeOut();
-                    $("#firstThing").fadeOut();
-                    $("#thirdThing").fadeIn();
-                });
-
-            });
-
-        </script>
+                })(jQuery);
+ 
+            </script>
         </div>
         <footer class="pageFoot">
             <div class="flex_cols">
@@ -137,7 +120,7 @@
                         </div>
                         <div class="content">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2982.3770409752533!2d-4.717704684502627!3d41.62598097924306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4712d844c78375%3A0x8532df1684bc7224!2sUniversidad+Europea+Miguel+de+Cervantes+-+UEMC!5e0!3m2!1ses!2ses!4v1547635284329" width="90%" height="80%" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
+                        </div>  
                     </div>
                 </div>
                 <div id="footer_copyright">
@@ -148,27 +131,35 @@
             </div>
         </footer>
     </body>
-</html>
     <?php
-        if( isset($_GET['passwdchange']) ){
-            if($_GET['passwdchange']==1){
+        if( isset($_GET['usrreg']) ){
+            if($_GET['usrreg']==1){
     ?>
     <script>
-        $('head').before('<div id="passwdchange" style="width: 100%; height: 20px; color: #56ed2d; background-color: #1e1e15; padding: 10px;">Contraseña cambiada con éxito</div>');
-        setTimeout(function(){
-            $('#passwdchange').fadeOut('fast');
+        $('head').before('<div id="usrreg" style="width: 100%; padding: 10px 10px; margin: 0; color: #000080; background-color: #e0e0d2;">Registrado con éxito, proceda a loguearse</div>');
+        setTimeout(function(){ 
+            $('#usrreg').fadeOut('fast');
             }, 4000
             );
     </script>
-<?php
+    <?php
             }
         }
-    }
+    ?>
+</html>
+<?php
+}
     else if( $_SERVER['REQUEST_METHOD']=='POST') {
-        if( $_POST['newPasswd'] == $_POST['newPasswd2'] && $usuario->changePasswd($_POST['newPasswd']) ){
-            header('Location: ?passwdchange=1');
+        $u = new Cliente();
+        $u  ->setUsername($_POST['id'])
+            ->setPasswd($_POST['passwd'])
+        ;
+        $u->encryptPasswd();
+        if( $u->login() ) {
+            header('Location: ../main/index.php?usrlog=1');
             exit;
-        }else {
+        }
+        else {
             echo "Error: Falló la operación";
         }
     }
