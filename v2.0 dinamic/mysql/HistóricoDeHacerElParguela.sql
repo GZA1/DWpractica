cc31137ed7611daa97b03c281ebc895d5c951499
Khalifa96 <gsenov@gmail.com>
2019-04-04 11:41:13 +0200

    Update SQL

--- v2.0 dinamic/mysql/HistóricoDeHacerElParguela.sql
+++ v2.0 dinamic/mysql/HistóricoDeHacerElParguela.sql
@@ -1,6 +1,3 @@
-INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
-VALUES(343434, 'papu', '123412341234123', 'Pepe', 'Hernandez Machado', 'papu@hotmail.com', 'Calle falsa 123', '240', '2018/07/07', '2018/09/09', NULL);
-
 SELECT * 
 FROM Cliente;
 
@@ -8,3 +5,23 @@ FROM Cliente;
 use BD_Tienda;
 
 drop database BD_Tienda;
+
+/*INSERTS CLIENTES*/
+
+INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
+VALUES(343434, 'papu', '123412341234123', 'Pepe', 'Hernandez Machado', 'papu@hotmail.com', 'Calle falsa 123', '240', '2018/07/07', '2018/09/09', NULL);
+
+
+INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
+VALUES(894589, 'Vito', 'adsasfsasfasfdf', 'Migue', 'Vito itoV', 'itoV@hotmail.com', 'Calle falsa 321', '666', '2015/07/07', '2018/10/09', NULL);
+
+INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
+VALUES(997778, 'Jaime', 'ubvue7764n', 'Pepe', 'Hernandez Machado', 'Jaime@hotmail.com', 'Calle falsa 13', '240', '2016/07/15', '2018/09/25', NULL);
+
+
+
+
+/*INSERTS EMPLEADOS*/
+
+
+
