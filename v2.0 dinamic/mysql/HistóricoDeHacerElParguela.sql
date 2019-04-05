5852abc0fbaa21818b593ba41497321aab21e365
miguelvitores <34660797+miguelvitores@users.noreply.github.com>
2019-04-05 20:14:18 +0200

    scripts y el modelo de la bd

--- v2.0 dinamic/mysql/HistóricoDeHacerElParguela.sql
+++ v2.0 dinamic/mysql/HistóricoDeHacerElParguela.sql
@@ -4,19 +4,25 @@ FROM Cliente;
 
 use BD_Tienda;
 
-drop database BD_Tienda;
+#drop database bd_tienda;
 
 /*INSERTS CLIENTES*/
 
-INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
-VALUES(343434, 'papu', '123412341234123', 'Pepe', 'Hernandez Machado', 'papu@hotmail.com', 'Calle falsa 123', '240', '2018/07/07', '2018/09/09', NULL);
+delete from Cliente
+where 1=1;
+show variables;
+set sql_safe_updates = 0;
 
 
-INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
-VALUES(894589, 'Vito', 'adsasfsasfasfdf', 'Migue', 'Vito itoV', 'itoV@hotmail.com', 'Calle falsa 321', '666', '2015/07/07', '2018/10/09', NULL);
+INSERT into Cliente(username, passwd, nombre, apellidos, email, domicilio, monedero, Cesta_id) 
+VALUES('papu', '123412341234123', 'Pepe', 'Hernandez Machado', 'papu@hotmail.com', 'Calle falsa 123', '240', NULL);
 
-INSERT into Cliente(id, username, passwd, nombre, apellidos, email, domicilio, monedero, fechaCreacion, fechaModificacion, Cesta_id) 
-VALUES(997778, 'Jaime', 'ubvue7764n', 'Pepe', 'Hernandez Machado', 'Jaime@hotmail.com', 'Calle falsa 13', '240', '2016/07/15', '2018/09/25', NULL);
+
+INSERT into Cliente(username, passwd, nombre, apellidos, email, domicilio, monedero, Cesta_id) 
+VALUES('Vito', 'adsasfsasfasfdf', 'Migue', 'Vito itoV', 'itoV@hotmail.com', 'Calle falsa 321', '666', NULL);
+
+INSERT into Cliente(username, passwd, nombre, apellidos, email, domicilio, monedero, Cesta_id) 
+VALUES('Jaime', 'ubvue7764n', 'Pepe', 'Hernandez Machado', 'Jaime@hotmail.com', 'Calle falsa 13', '240', NULL);
 
 
 
