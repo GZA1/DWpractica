972267113bfa5b1cdd95cc3310255005c06f07b3
Khalifa96 <gsenov@gmail.com>
2019-06-01 00:28:20 +0200

    nuse

--- v3.0 Doctrine/htdocs/tienda-dw/src/Repository/CategoriaRepostory.php
+++ v3.0 Doctrine/htdocs/tienda-dw/src/Repository/CategoriaRepostory.php
@@ -7,6 +7,20 @@ use Doctrine\ORM\EntityRepository;
 
 class CategoriaRepository extends EntityRepository
 {
+    public function findProductosPrecioGT50()
+    {
+        $DQL = "select p from Entity\\Producto p where p.precio > 50";
+        $query = $this->_em->createQuery($DQL);
+        $productosGT50 = $query->getResult();  
+        return $productosGT50;        
+    }
+
+
+
+
+
+
+
     
 }
 
--- v3.0 Doctrine/htdocs/tienda-dw/src/Repository/EmpleadoRepository.php
+++ v3.0 Doctrine/htdocs/tienda-dw/src/Repository/EmpleadoRepository.php
@@ -7,7 +7,23 @@ use Doctrine\ORM\EntityRepository;
 
 class EmpleadoRepository extends EntityRepository
 {
+    public function findProductosPrecioGT50()
+    {
+        $DQL = "select p from Entity\\Producto p where p.precio > 50";
+        $query = $this->_em->createQuery($DQL);
+        $productosGT50 = $query->getResult();  
+        return $productosGT50;        
+    }
     
+    public function getEmpleadoByID($username, $passwd, $nombre, $apell, $email, $photoPath, $active, $cargo, $isAdministrador, $tienda_id){
+        
+        $DQL = "select * from Entity\\Empleado where id = :id";
+        $query = -> $this ->createQuery($DQL);
+        $query->setParameters('id' => 'Bob');
+        $emp = $query->_em->getResult();
+    }
+
+
 }
 
 ?>
