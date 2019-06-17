9b62c59f9f5f98456bfc0bed48e660fb0e5ea693
Khalifa96 <gsenov@gmail.com>
2019-06-18 01:36:41 +0200

    confs
    Configuracion de los formularios, javascript de estos y los parámetros del $_GET y etc

--- v3.0 Doctrine/htdocs/tienda-dw/catalogo/cfgCatalogo.php
+++ v3.0 Doctrine/htdocs/tienda-dw/catalogo/cfgCatalogo.php
@@ -49,6 +49,9 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
+        
+        
+        <script src="./checkcfgCatalogo.js"></script>
         <script type="text/javascript">
            $("document").ready(function(){
             $("#optAddCAT").click(function(){
@@ -142,10 +145,10 @@
                         <input type="text" id="nombreCatAdd" name="nombreCATAdd">
                         <label id="newAcronimo">Acrónimo</label>
                         <input type="text" id="acrCatAdd" name="acrCATAdd">
-                        <label id="">Descripción</label>
+                        <label id="newDescripcion">Descripción</label>
                         <input type="text" id="descCatAdd" name="descCATAdd">
 
-                        <label id="aCPasswdConfirm">Introduzca su contraseña para confirmar</label>
+                        <label id="aClPasswdConfirm">Introduzca su contraseña para confirmar</label>
                         <input type="password" placeholder="Contraseña" name="ContraseñaConfirm" id="aCpasswdConfirm">
                         <input class="submitCDF" type="submit" name="optsSubmit2" id="addCategoria" value="Añadir Categoria">
                         <input class="submitCDF cancel" id="cancelButtonACAT" type="button" value="Cancelar">
@@ -165,14 +168,14 @@
                             }
                             ?>        
                         </select>
-                        <label id="newNombre">Nombre de categoria</label>
+                        <label id="editNombre">Nombre de categoria</label>
                         <input type="text" id="nombreCatEdit" name="nombreCATEdit">
-                        <label id="newAcronimo">Acrónimo</label>
+                        <label id="editAcronimo">Acrónimo</label>
                         <input type="text" id="acrCatEdit" name="acrCATEdit">
-                        <label id="">Descripción</label>
+                        <label id="editDescripcion">Descripción</label>
                         <input type="text" id="descCatEdit" name="descCATEdit">
 
-                        <label id="eCPasswdConfirm">Introduzca su contraseña para confirmar</label>
+                        <label id="eClPasswdConfirm">Introduzca su contraseña para confirmar</label>
                         <input type="password" placeholder="Contraseña" name="ContraseñaConfirm" id="eCpasswdConfirm">
                         <input class="submitCDF" type="submit" name="optsSubmit2" id="editCategoria" value="Editar Categoria">
                         <input class="submitCDF cancel" id="cancelButtonECAT" type="button" value="Cancelar">
@@ -225,7 +228,7 @@
                                 }
                             ?>
                         </select>
-                        <label id="aPPasswdConfirm">Introduzca su contraseña para confirmar</label>
+                        <label id="aPlPasswdConfirm">Introduzca su contraseña para confirmar</label>
                         <input type="password" placeholder="Contraseña" name="ContraseñaConfirm" id="aPpasswdConfirm">
                         <input class="submitCDF" type="submit" name="optsSubmit2" id="addProducto" value="Añadir Producto">
                         <input class="submitCDF cancel" id="cancelButtonAPROD" type="button" value="Cancelar">
@@ -257,7 +260,7 @@
                             }
                         ?>
                         </select>
-                        <label id="ePPasswdConfirm">Introduzca su contraseña para confirmar</label>
+                        <label id="ePlPasswdConfirm">Introduzca su contraseña para confirmar</label>
                         <input type="password" placeholder="Contraseña" name="ContraseñaConfirm" id="ePpasswdConfirm">
                         <input class="submitCDF" type="submit" name="optsSubmit2" id="editProducto" value="Editar Producto">
                         <input class="submitCDF cancel" id="cancelButtonEPROD" type="button" value="Cancelar">
@@ -277,7 +280,7 @@
                                 }
                                 ?>        
                         </select> 
-                        <label id="rPPasswdConfirm">Introduzca su contraseña para confirmar</label>
+                        <label id="rPlPasswdConfirm">Introduzca su contraseña para confirmar</label>
                         <input type="password" placeholder="Contraseña" name="ContraseñaConfirm" id="rPpasswdConfirm">
                         <input class="submitCDF" type="submit" name="optsSubmit2" id="updateButton" value="Eliminar Producto">
                         <input class="submitCDF cancel" id="cancelButtonRPROD" type="button" value="Cancelar">
@@ -289,11 +292,105 @@
 
 </html>
 <?php
+        if(isset($_GET['addCat'])){
+           if($_GET['addCat'] == 1){
+    ?>
+        <script type="text/javascript">
+            $('head').before('<div id="addCat" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria añadida</div>');        
+            setTimeout(function(){
+                $('#addCat').fadeOut('fast');
+                }, 4000
+                );        
+        </script>  
+<?php            
+            }
+        }else if(isset($_GET['editCat'])){
+            if($_GET['editCat'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="editCat" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria editada</div>');        
+                setTimeout(function(){
+                    $('#editCat').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }else if(isset($_GET['remCat'])){
+            if($_GET['remCat'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="remCat" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria editada</div>');        
+                setTimeout(function(){
+                    $('#remCat').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }else if(isset($_GET['addPr'])){
+            if($_GET['addPr'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="addPr" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria editada</div>');        
+                setTimeout(function(){
+                    $('#addPr').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }else if(isset($_GET['editPr'])){
+            if($_GET['editPr'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="editPr" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria editada</div>');        
+                setTimeout(function(){
+                    $('#editPr').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }else if(isset($_GET['removePr'])){
+            if($_GET['removePr'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="removePr" style="width: 100%; height: 20px; color: #3ca51f; font-weight: 900; background-color: #e0e0d2; padding: 10px;">Categoria editada</div>');        
+                setTimeout(function(){
+                    $('#removePr').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }else if(isset($_GET['opfallida'])){
+            if($_GET['opfallida'] == 1){
+?>
+            <script type="text/javascript">
+                $('head').before('<div id="opfallida" style="width: 100%; height: 20px; color: #ff7f7f; background-color: #e0e0d2; padding: 10px;">Operación fallida</div>');        
+                setTimeout(function(){
+                    $('#opfallida').fadeOut('fast');
+                    }, 4000
+                    );        
+            </script>
+<?php 
+            }
+        }
+
+
+
+
+
+
+<?php
 }else if( $_SERVER['REQUEST_METHOD']=='POST') {
     console_log('POST');
         console_log($_POST['optsSubmit2']);
         if(isset($_POST['optsSubmit2'])){
+            
             switch($_POST['optsSubmit2']){
+
                 case 'Añadir Categoria':
                     $nuevaCategoria = new Categoria();
                     $nuevaCategoria->setNombre($_POST['nombreCATAdd'])
@@ -305,14 +402,26 @@
                 break;
 
                 case 'Editar Categoria':                
-                
-                    $catEdit = $em->findOneBy('acronimo', $_POST['cat_elegEdit']);
-                    $catEdit->setNombre($_POST['nombreCATEdit'])
-                            ->setAcronimo($_POST['acrCATEdit'])
-                            ->setDescripcion($_POST['descCATEdit']);
+                $catEdit = $em->findOneBy('acronimo', $_POST['cat_elegEdit']);
+                if(isset($catEdit)){
+                    
+                    if($_POST['nombreCATEdit'] != ""){
+                        $catEdit->setNombre($_POST['nombreCATEdit']);
+                    }                    
+                    if($_POST['acrCATEdit'] != ""){
+                        $catEdit->setAcronimo($_POST['acrCATEdit']);
+                    }
+                    if($_POST['descCATEdit'] != ""){
+                        $catEdit->setDescripcion($_POST['descCATEdit']);
+                    }
+
                     $em->persist($catEdit);
                     $em->flush();
                     header('Location: ?editCat=1');
+                }else{
+                    
+                    header('Location: ?opfallida=1');
+                }
                 
                 break;
 
@@ -322,6 +431,7 @@
                     $em->flush();
                     header('Location: ?remCat=1');
                 break;
+
                 case 'Añadir Producto':
                     $nuevoProducto = new Producto();
                     $nuevoProducto->setNombre($_POST['nombrePrNEW'])
@@ -339,17 +449,30 @@
                 case 'Editar Producto':
                 $catEdit = $em->findOneBy('acronimo', $_POST['cat_elegEdit']);
                 if(isset($prEdit)){
-                    $prEdit->setNombre($_POST['nombrePrEDIT'])
-                    ->setMarca($_POST['marcaPrEDIT'])
-                    ->setModelo($_POST['modeloPrEDIT'])
-                    ->setPrecio($_POST['precioPrEDIT'])
-                    ->setDescripcion($_POST['descPrEDIT'])
-                    ->setPicPath($_POST['picpathPrEDIT']);
+                    if($_POST['nombrePrEDIT'] != ""){
+                        $prEdit->setNombre($_POST['nombrePrEDIT']);
+                    }                    
+                    if($_POST['marcaPrEDIT'] != ""){
+                        $prEdit->setMarca($_POST['marcaPrEDIT']);
+                    }
+                    if($_POST['modeloPrEDIT'] != ""){
+                        $prEdit->setModelo($_POST['modeloPrEDIT']);
+                    }
+                    if($_POST['precioPrEDIT'] != ""){
+                        $prEdit->setPrecio($_POST['precioPrEDIT']);
+                    }
+                    if($_POST['descPrEDIT'] != ""){
+                        $prEdit->setDescripcion($_POST['descPrEDIT']);
+                    }
+                    if($_POST['picpathPrEDIT'] != ""){
+                        $prEdit->setPicPath($_POST['picpathPrEDIT']);
+                    }
+
                     $em->persist($prEdit);
                     $em->flush();
                     header('Location: ?editPr=1');
                 }else{
-                    header('Location: ?editPr=0');
+                    header('Location: ?opfallida=1');
                 }
                 
                 break;
---
+++ v3.0 Doctrine/htdocs/tienda-dw/catalogo/checkcfgCatalogo.js
@@ -0,0 +1,168 @@
+(function($){
+
+
+    $('#aCAT').submit(function(){
+        console.log("vamos a comprobar los campos de esta categoria");
+        $("#errorCat").remove();
+        var nombreCategoria = $("#nombreCatAdd").val(), 
+            acronimoCategoria = $("#acrCatAdd").val(),
+            descripcionCategoria = $("#descCatAdd").val(),
+            passwdConfirm = $("#aCpasswdConfirm").val();
+        var inputVal = [nombreCategoria, acronimoCategoria, descripcionCategoria, passwdConfirm],
+            inputMessage = ["nombre catálogo", "acrónimo catálogo", "descripción catálogo", "contraseña de confirmación"],
+            textId = ["#newNombre", "#newAcronimo", "#newDescripcion", "#aClPasswdConfirm"];
+        for(var i=0;i<inputVal.length;i++){
+            inputVal[i] = $.trim(inputVal[i]);
+            console.log(inputVal[i]);
+            if ( inputVal[i] == null || inputVal[i] === "") {
+                console.log(inputVal[i] + ' incorrecto');
+                invalidEntry(i);
+                return false;
+            }
+        }
+        function invalidEntry(i) {
+            console.log(textId[i] + ' incorrecto');
+            $(textId[i]).after("<p id='error' style='font-size: 14px; color: red' > El campo " + inputMessage[i] + " no es válido.</p>");
+        }
+        console.log("campos correctos - Categoria añadida");
+        return true;
+
+    });
+    
+    $("input[type='text']").change(function() {
+        $("#error").remove();
+    });
+
+
+
+
+    
+    $('#eCAT').submit(function(){
+        console.log("vamos a comprobar la contraseña para poder editar categoria");
+        $("#errorCat").remove();
+        var passwdConfirm = $("#eCpasswdConfirm").val();
+        var textId = "#eClPasswdConfirm";
+        
+        if ( passwdConfirm == null || passwdConfirm === "") {
+            console.log('contraseña incorrecto');
+            invalidEntry("contraseña");
+            return false;
+        }
+
+        function invalidEntry(i) {
+            console.log(i + ' incorrecta');
+            $(textId).after("<p id='error' style='font-size: 14px; color: red' > El campo " + i + " no es válido.</p>");
+        }  
+        console.log("Pass correcta al editar categoria");
+        return true;
+    });
+
+
+
+
+
+    $('#rCAT').submit(function(){
+        console.log("vamos a comprobar la contraseña para poder eliminar categoria");
+        $("#errorCat").remove();
+        var passwdConfirm = $("#eCpasswdConfirm").val();
+        var textId = "#eClPasswdConfirm"; 
+
+        if ( passwdConfirm == null || passwdConfirm === "") {
+            console.log(passwdConfirm + ' incorrecto');
+            invalidEntry(4);
+            return false;
+        }
+
+        function invalidEntry(i) {
+            console.log(i + ' incorrecta');
+            $(textId).after("<p id='error' style='font-size: 14px; color: red' > El campo " + i + " no es válido.</p>");
+        }  
+
+        console.log("Pass correcta al eliminar categoria");
+        return true;
+    });
+
+
+
+    $('#aPROD').submit(function(){
+        console.log("vamos a comprobar los campos de este empleado");
+        $("#errorCat").remove();
+        var nombreProducto = $("#nombrePrNuevo").val(), 
+            marcaProducto = $("#marcaPrNuevo").val(),
+            modeloProducto = $("#modeloPrNuevo").val(),
+            precioProducto = $("#precioPrNuevo").val();
+            descripcionProducto = $("#descripcionPrNuevo").val(),
+            picpathProducto = $("#picpathPrNuevo").val();
+            passwdConfirm = $("#aPlpasswdConfirm").val();
+        var inputVal = [nombreProducto, marcaProducto, modeloProducto, precioProducto, passwdConfirm],
+            inputMessage = ["nombre producto", "marca producto", "modelo producto", "precioProducto", "descripcionProducto", "picpathProducot"],
+            textId = ["#nPrNombre", "#nPrMarca", "#nPrModelo", "nPrPrecio", "nPrDescripcion", "nPrPicPath", "#aPlPasswdConfirm"];
+            
+
+        for(var i=0;i<inputVal.length;i++){
+            inputVal[i] = $.trim(inputVal[i]);
+            console.log(inputVal[i]);
+            if ( inputVal[i] == null || inputVal[i] === "") {
+                console.log(inputVal[i] + ' incorrecto');
+                invalidEntry(i);
+                return false;
+            }
+        }
+        
+        function invalidEntry(i) {
+            console.log(textId[i] + ' incorrecto');
+            $(textId[i]).after("<p id='error' style='font-size: 14px; color: red' > El campo " + inputMessage[i] + " no es válido.</p>");
+        }  
+        console.log("campos correctos - Producto añadido");
+        return true;
+    });
+
+
+
+    $('#ePROD').submit(function(){
+        console.log("vamos a comprobar los campos de este empleado");
+        $("#errorCat").remove();
+        var passwdConfirm = $("#ePpasswdConfirm").val();
+        var textId = "#ePPasswdConfirm";
+            
+            if ( passwdConfirm == null || passwdConfirm === "") {
+                console.log(passwdConfirm + ' incorrecto');
+                invalidEntry("contraseña");
+                return false;
+            }
+    
+            function invalidEntry(i) {
+                console.log(i + ' incorrecta');
+                $(textId).after("<p id='error' style='font-size: 14px; color: red' > El campo " + i + " no es válido.</p>");
+            }  
+            console.log("Pass correcta al editar Producto");
+        return true;
+    });
+
+
+
+
+    $('#rPROD').submit(function(){
+        console.log("vamos a comprobar los campos de este empleado");
+        $("#errorCat").remove();
+        var passwdConfirm = $("#aPpasswdConfirm").val();
+        var textId = "#rPlPasswdConfirm";
+            
+            if ( passwdConfirm == null || passwdConfirm === "") {
+                console.log(passwdConfirm + ' incorrecto');
+                invalidEntry("contraseña");
+                return false;
+            }
+    
+            function invalidEntry(i) {
+                console.log(i + ' incorrecta');
+                $(textId).after("<p id='error' style='font-size: 14px; color: red' > El campo " + i + " no es válido.</p>");
+            }  
+            console.log("Pass correcta al eliminar Producto");
+        return true;
+    });
+
+
+
+
+});
